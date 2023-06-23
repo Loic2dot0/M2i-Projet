192924943d329b4d189f2981312fea8ba8aba042
Loic2dot0 <moreaux.loic@gmail.com>
2023-06-23 10:49:22 +0200

    ajout des insert modification des scripts de création des tables

--- sql/project_database.sql
+++ sql/project_database.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS m2i_project;
+
 /* Creation database if not exist */
 CREATE DATABASE IF NOT EXISTS m2i_project;
 USE m2i_project;
@@ -5,7 +7,7 @@ USE m2i_project;
 /* Creation table clients */
 CREATE TABLE IF NOT EXISTS clients(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    company_name VARCHAR(255),
+    company_name VARCHAR(255) NOT NULL,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     email VARCHAR(255),
@@ -20,11 +22,30 @@ CREATE TABLE IF NOT EXISTS clients(
 /* creation table orders */
 CREATE TABLE IF NOT EXISTS orders(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    type_presta VARCHAR(255),
-    designation	VARCHAR(255),
-    client_id INT,
-    nb_days INT,
-    unit_price FLOAT,
-    state TINYINT,
+    type_presta VARCHAR(255) NOT NULL,
+    designation	VARCHAR(255) NOT NULL,
+    client_id INT NOT NULL,
+    nb_days INT DEFAULT NULL,
+    unit_price FLOAT DEFAULT NULL,
+    state TINYINT DEFAULT NULL,
     FOREIGN KEY(client_id) REFERENCES clients(id)
 );
+
+/* insert clients */
+
+
+
+
+/* insert data */
+INSERT INTO orders
+    VALUES
+        ('Formation', 'Angular init', 2, 3, 1200, 0),
+        ('Formation', 'React avancé', 2, 3, 1000, 2),
+        ('Coaching', 'React Techlead', 1, 20, 900, 2),
+        ('Coaching', 'Nest.js Techlead', 1, 50, 800, 1);
+
+INSERT INTO orders (type_presta, designation, client_id)
+    VALUES
+        ('Coaching', 'React Techlead', 3),
+        ('Coaching', 'Jakarta EE', 3),
+        ('Coaching', 'Angular Teachlead', 4);
