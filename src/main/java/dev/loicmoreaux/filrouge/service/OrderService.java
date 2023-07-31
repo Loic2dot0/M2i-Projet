8a55515d69dbf66d790779c82758d9835877fa43
Loic2dot0 <moreaux.loic@gmail.com>
2023-07-31 11:06:52 +0200

    add order service

---
+++ src/main/java/dev/loicmoreaux/filrouge/service/OrderService.java
@@ -0,0 +1,38 @@
+package dev.loicmoreaux.filrouge.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dev.loicmoreaux.filrouge.dao.OrderRepository;
+import dev.loicmoreaux.filrouge.entity.Order;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	OrderRepository orderRepository;
+	
+	public List<Order> getAllOrders(){
+		return orderRepository.findAll();
+	}
+	
+	public Optional<Order> getOrderById(Long id){
+		return orderRepository.findById(id);
+	}
+	
+	public Order saveOrder(Order order) {
+		return orderRepository.save(order);
+	}
+	
+	public Order updateOrder(Order order) {
+		return orderRepository.save(order);
+	}
+	
+	public void deleteOrder(Long id) {
+		orderRepository.deleteById(id);
+	}
+	
+}
